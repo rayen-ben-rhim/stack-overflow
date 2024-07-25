17b8b69dc59c56a306cfc59126ac7719d8a1e871
rayen ben rhim <139109664+rayen-ben-rhim@users.noreply.github.com>
2024-07-25 16:21:22 +0100

    add auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { UserButton } from '@clerk/nextjs'
 
 
-const page = () => {
+const Home = () => {
   return (
     <div>
+        <UserButton afterSignOutUrl='/'/>
       <h1 className='h1-bold'> hello </h1>
       <h2 className='h2-bold'>hello</h2>
     </div>
   )
 }
 
-export default page
+export default Home
---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AskQ = () => {
+  return (
+    <div>AskQ</div>
+  )
+}
+
+export default AskQ
