2b9e45bfd5fa6c6b59035ebbd98c6b299e75c92a
rayen ben rhim <139109664+rayen-ben-rhim@users.noreply.github.com>
2024-07-25 12:06:31 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='h1-bold'> hello </h1>
+      <h2 className='h2-bold'>hello</h2>
+    </div>
+  )
+}
+
+export default page
